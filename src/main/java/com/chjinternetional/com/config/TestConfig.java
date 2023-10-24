81998d6a0104474935d7d3c02c5bffb06bd88c3e
Cindy Jelinsky <cindyjelinsky@yahoo.com.br>
2023-10-24 07:18:07 -0300

    OrderStatus

--- src/main/java/com/chjinternetional/com/config/TestConfig.java
+++ src/main/java/com/chjinternetional/com/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.chjinternetional.com.entities.Order;
 import com.chjinternetional.com.entities.User;
+import com.chjinternetional.com.entities.status.OrderStatus;
 import com.chjinternetional.com.repositories.OrderRepository;
 import com.chjinternetional.com.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.valueOf(1), u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.valueOf(2), u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.valueOf(3), u1);
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
